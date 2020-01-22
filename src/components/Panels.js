8529e9d450fb9e27f21a1cd3a6f91acaac06dff8
Nick Redmark <nicolamr@gmail.com>
2020-01-23 00:13:16 +0100

    feat: Better handling of frames

--- src/components/Panels.js
+++ src/components/Panels.js
@@ -6,17 +6,23 @@ export const Panels = ({ url }) => {
     const onMessage = e => {
       switch (e.data.type) {
         case "open-child":
-          const frame = +e.data.name.substr(6);
-          setFrames(frames => [
-            ...frames.slice(0, frame + 1),
-            {
-              url:
-                e.data.message && frames.length > frame + 1
-                  ? frames[frame + 1].url
-                  : e.data.url,
-              message: e.data.message
+          setFrames(frames => {
+            const frame = +e.data.name.substr(6);
+            const newFrames = frames.slice(0, frame + 1);
+            let url = e.data.url;
+            if (e.data.message && frames[frame + 1]) {
+              const urlObject = new URL(url);
+              const frameUrlObject = new URL(frames[frame + 1].url);
+              if (
+                urlObject.origin + urlObject.pathname ===
+                frameUrlObject.origin + frameUrlObject.pathname
+              ) {
+                url = frames[frame + 1].url;
+              }
             }
-          ]);
+            newFrames.push({ url, message: e.data.message });
+            return newFrames;
+          });
           break;
       }
     };
@@ -36,7 +42,6 @@ const Panel = ({ url, message, i }) => {
   const ref = useRef(null);
   useEffect(() => {
     if (ref.current && message) {
-      console.log("posting message");
       ref.current.contentWindow.postMessage(message, new URL(url).origin);
     }
   }, [ref.current, message]);
